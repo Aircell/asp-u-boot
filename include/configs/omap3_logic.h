0387bfa1d190fbbd4bc9e9dfeb5c403bf2242519
John Kelly <jkelly@aircell.com>
2012-03-16 18:36:46 -0600

    Tidy up u-boot welcome message

--- include/configs/omap3_logic.h
+++ include/configs/omap3_logic.h
@@ -211,7 +211,7 @@
 	"memsize=126M\0" \
 	"nfsboot=bootp; setenv bootargs mem=${memsize} omapfb.vrfb=${rotate_type} omapfb.rotate=${rotate} omapfb.debug=${display_debug} console=${consoledev},${baudrate} root=/dev/nfs rw nfsroot=${rootpath}${nfsoptions} ip=dhcp init=/init androidboot.console=${consoledev} ${otherbootargs};bootm ${loadaddr}\0" \
 	"flashboot=setenv bootargs mem=${memsize} omapfb.vrfb=${rotate_type} omapfb.rotate=${rotate} omapfb.debug=${display_debug} console=${consoledev},${baudrate} root=/dev/mtdblock4 rootfstype=yaffs2 rw init=/init androidboot.console=${consoledev} ${otherbootargs}; nand read ${loadaddr} 280000 400000; bootm ${loadaddr}\0" \
-	"netprep=mmc init; fatload mmc 0 80000000 netboot.img; source\0" \
+	"install=mmc init; fatload mmc 0 80000000 install.img; source\0" \
     "sdboot=setenv bootargs mem=${memsize} omapfb.vrfb=${rotate_type} omapfb.rotate=${rotate} omapfb.debug=${display_debug} console=${consoledev},${baudrate} root=/dev/mmcblk0p2 rootfstype=ext3 rw rootwait init=/init; mmc init; fatload mmc 0 ${loadaddr} ${kernelimage}; bootm ${loadaddr}\0"
 
 //#define CONFIG_BOOTCOMMAND "run nfsboot"
@@ -221,14 +221,16 @@
 #define CONFIG_PREBOOT \
 	"echo ==========================NOTICE============================;"    \
 	"echo ;" \
-	"echo Type 'run netprep'     to prepare the flash for netbooting;" \
-	"echo Type 'run flashboot'  to boot from flash;" \
-	"echo Type 'run sdtboot'     to boot from sdcard;" \
+	"echo Type 'run install'     to install flash from sd;" \
+	"echo Type 'run flashboot'   to boot from flash;" \
+	"echo Type 'run sdboot'      to boot from sdcard;" \
 	"echo Type 'run nfsboot'     to boot from the network;" \
 	"echo ;" \
 	"echo Use 'setenv bootcmd' to set your default, which is currently: ;" \
 	"printenv bootcmd ;"  \
-	"echo Then run 'saveenv' to make your changes persistent;" \
+	"echo Then run 'saveenv' to make your changes persistent, for example:;" \
+	"echo Booting _NFS_: setenv bootcmd run nfsboot   \\; saveenv;" \
+	"echo Booting Flash: setenv bootcmd run flashboot \\; saveenv;" \
 	"echo ==========================******============================;" 
 
 	
